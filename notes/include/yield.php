6468f5a5005b58b7c74f0184edfb32e8fe3ac77a
VIN <115197877+Binn12345@users.noreply.github.com>
2024-01-24 17:37:11 +0800

    temporary push

--- notes/include/yield.php
+++ notes/include/yield.php
@@ -17,7 +17,7 @@
         <div class="col-lg-12">
           <div class="row">
            <!-- Sales Card -->
-           <div class="col-xxl-4 col-md-6">
+           <div class="col-xxl-12 col-md-6">
               <div class="card info-card sales-card">
 
                 <div class="filter">
